89a805c63e75e216a767d389238e4198ad41282f
Marat Minchik <devminrat@gmail.com>
2021-04-18 22:26:23 +0300

    added main functional

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,5 +1,42 @@
 "use strict";
 
-$(function () {
-  console.log("file 3");
+document.addEventListener("DOMContentLoaded", function () {
+  var result = document.querySelector("#result"),
+      box = document.querySelector(".box"),
+      wrapper = document.querySelector(".box-wrapper"),
+      unitCheckbox = document.querySelector("#value"),
+      topLeftAngle = document.querySelector("#top-left-angle"),
+      topRightAngle = document.querySelector("#top-right-angle"),
+      bottomRightAngle = document.querySelector("#bottom-right-angle"),
+      bottomLeftAngle = document.querySelector("#bottom-left-angle"),
+      resultCopyBtn = document.querySelector(".result-copy-btn");
+  var btlr,
+      btrr,
+      bblr,
+      bbrr,
+      unit = "px";
+
+  function changeInputValues() {
+    btlr = topLeftAngle.value ? topLeftAngle.value + unit : "0px";
+    btrr = topRightAngle.value ? topRightAngle.value + unit : "0px";
+    bbrr = bottomRightAngle.value ? bottomRightAngle.value + unit : "0px";
+    bblr = bottomLeftAngle.value ? bottomLeftAngle.value + unit : "0px";
+    box.style.borderRadius = "".concat(btlr, " ").concat(btrr, " ").concat(bbrr, " ").concat(bblr);
+    result.value = "border-radius: ".concat(btlr, " ").concat(btrr, " ").concat(bbrr, " ").concat(bblr);
+  }
+
+  wrapper.addEventListener("input", changeInputValues);
+  unitCheckbox.addEventListener("change", function () {
+    unit = unitCheckbox.checked ? "px" : "%";
+    changeInputValues();
+  });
+
+  function copyResult() {
+    result.select();
+    result.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Скопировал текст: " + result.value);
+  }
+
+  resultCopyBtn.addEventListener("click", copyResult);
 });
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,45 @@
-$(function () {
-  console.log("file 3");
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const result = document.querySelector("#result"),
+    box = document.querySelector(".box"),
+    wrapper = document.querySelector(".box-wrapper"),
+    unitCheckbox = document.querySelector("#value"),
+    topLeftAngle = document.querySelector("#top-left-angle"),
+    topRightAngle = document.querySelector("#top-right-angle"),
+    bottomRightAngle = document.querySelector("#bottom-right-angle"),
+    bottomLeftAngle = document.querySelector("#bottom-left-angle"),
+    resultCopyBtn = document.querySelector(".result-copy-btn");
+
+  let btlr,
+    btrr,
+    bblr,
+    bbrr,
+    unit = "px";
+
+  function changeInputValues() {
+    btlr = topLeftAngle.value ? topLeftAngle.value + unit : "0px";
+    btrr = topRightAngle.value ? topRightAngle.value + unit : "0px";
+    bbrr = bottomRightAngle.value ? bottomRightAngle.value + unit : "0px";
+    bblr = bottomLeftAngle.value ? bottomLeftAngle.value + unit : "0px";
+
+    box.style.borderRadius = `${btlr} ${btrr} ${bbrr} ${bblr}`;
+    result.value = `border-radius: ${btlr} ${btrr} ${bbrr} ${bblr}`;
+  }
+
+  wrapper.addEventListener("input", changeInputValues);
+
+  unitCheckbox.addEventListener("change", () => {
+    unit = unitCheckbox.checked ? "px" : "%";
+    changeInputValues();
+  });
+
+  function copyResult() {
+    result.select();
+    result.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Скопировал текст: " + result.value);
+  }
+
+  resultCopyBtn.addEventListener("click", copyResult);
 });
